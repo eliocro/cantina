9124fae467ff6de4073b9e5a3d7a3dc472adacbe
Élio Cró <elio.cro@gmail.com>
2024-11-11 17:23:28 +0000

    Add styled components and add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import useSearch from './hooks/useSearch';
 import useSearchParams from './hooks/useSearchParams';
 
+import Layout from './components/Layout';
 import CharacterTable from './components/CharacterTable';
 import SearchField from './components/SearchField';
 import Pagination from './components/Pagination';
@@ -10,12 +11,12 @@ function App() {
   const { data, count } = useSearch({ text, page });
 
   return (
-    <>
-      <h1>Star Wars Cantina</h1>
+    <Layout>
+      <h1>Character Search</h1>
       <SearchField initial={text} onSubmit={setText} />
       <CharacterTable items={data} />
       <Pagination count={count} page={page} setPage={setPage} />
-    </>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+type Props = { children: ReactNode };
+
+export default function Layout({ children }: Props) {
+  return (
+    <StyledDiv>
+      <header>
+        <img src="/logo.svg" alt="Star Wars" />
+      </header>
+      <main>{children}</main>
+    </StyledDiv>
+  );
+}
+
+const StyledDiv = styled.div`
+  padding: 0 1.5rem 2rem;
+  height: 100%;
+  overflow: auto;
+
+  background-color: #000;
+  background-image: url(/stars.jpg);
+
+  > header,
+  main {
+    margin: 2rem auto;
+    max-width: calc(1024px - 3rem);
+  }
+
+  > header img {
+    max-width: 80%;
+  }
+
+  main {
+    padding: 2rem 1.5rem;
+    border-radius: 0.5rem;
+    color: var(--fg);
+    background-color: var(--bg);
+  }
+`;
